a540bb799a7201ffe08e931f2f4ecfb8f6750372
bigfishdesign13 <marty@brutvan.com>
2025-02-19 18:05:50 -0500

    more config for prettier and vscode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import { css } from '../../styled-system/css';
+import { css } from "../../styled-system/css";
 // import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "3xl", fontWeight: 'bold' })}>
+    <div
+      className={css({
+        fontSize: "3xl",
+        fontWeight: "bold",
+        color: "red",
+        padding: "32px",
+      })}
+    >
       Code Playground
     </div>
   );
